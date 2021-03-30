2cf29932e0e08037913b093628fdc8566898aa69
Halo-Michael <yc992147156@me.com>
2021-03-30 19:19:25 +0800

    Update

--- ResolutionSetterRootListController.m
+++ ResolutionSetterRootListController.m
@@ -1,20 +1,7 @@
 #import <Preferences/PSListController.h>
 #import <Preferences/PSSpecifier.h>
 
-UIAlertController *alert(NSString *alertTitle, NSString *alertMessage, NSString *actionTitle) {
-    UIAlertController *theAlert = [UIAlertController
-                                alertControllerWithTitle:alertTitle
-                                message:alertMessage
-                                preferredStyle:UIAlertControllerStyleAlert];
-    UIAlertAction *defaultAction = [UIAlertAction
-                                    actionWithTitle:actionTitle
-                                    style:UIAlertActionStyleDefault
-                                    handler:^(UIAlertAction * action) {}];
-    [theAlert addAction:defaultAction];
-    return theAlert;
-}
-
-bool heightError = false, widthError = false, hasSetHeight = false, hasSetWidth = false;
+bool hasSetHeight = false, hasSetWidth = false;
 
 @interface ResolutionSetterRootListController : PSListController
 
@@ -31,46 +18,24 @@ bool heightError = false, widthError = false, hasSetHeight = false, hasSetWidth 
 
 - (void)setPreferenceValue:(id)value specifier:(PSSpecifier*)specifier {
     if ([[specifier propertyForKey:@"key"] isEqualToString:@"canvas_height"]) {
-        if ([value intValue] < 100) {
-            heightError = true;
-            [self presentViewController:alert(@"Error!", [NSString stringWithFormat:@"Wrong input \"%@\":\nHeight is too low!", value], @"OK") animated:YES completion:nil];
-            return;
-        } else if ([value intValue] > 9999) {
-            heightError = true;
-            [self presentViewController:alert(@"Error!", [NSString stringWithFormat:@"Wrong input \"%@\":\nHeight is too high!", value], @"OK") animated:YES completion:nil];
-            return;
-        } else {
-            heightError = false;
-            hasSetHeight = true;
-        }
+        hasSetHeight = true;
     }
     if ([[specifier propertyForKey:@"key"] isEqualToString:@"canvas_width"]) {
-        if ([value intValue] < 100) {
-            widthError = true;
-            [self presentViewController:alert(@"Error!", [NSString stringWithFormat:@"Wrong input \"%@\":\nWidth is too low!", value], @"OK") animated:YES completion:nil];
-            return;
-        } else if ([value intValue] > 9999) {
-            widthError = true;
-            [self presentViewController:alert(@"Error!", [NSString stringWithFormat:@"Wrong input \"%@\":\nWidth is too high!", value], @"OK") animated:YES completion:nil];
-            return;
-        } else {
-            widthError = false;
-            hasSetWidth = true;
-        }
+        hasSetWidth = true;
     }
     [super setPreferenceValue:value specifier:specifier];
 }
 
 - (id)readPreferenceValue:(PSSpecifier*)specifier {
     if ([[specifier propertyForKey:@"key"] isEqualToString:@"canvas_height"] || [[specifier propertyForKey:@"key"] isEqualToString:@"canvas_width"]) {
-        return (id)CFBridgingRelease(CFPreferencesCopyValue((__bridge CFStringRef)[specifier propertyForKey:@"key"], CFSTR("com.apple.iokit.IOMobileGraphicsFamily"), CFSTR("mobile"), kCFPreferencesAnyHost));
+        return [[[NSUserDefaults alloc] _initWithSuiteName:@"com.apple.iokit.IOMobileGraphicsFamily" container:[NSURL URLWithString:@"file:///private/var/mobile"]] objectForKey:[specifier propertyForKey:@"key"]];
     }
     return [super readPreferenceValue:specifier];
 }
 
 -(void)setresolution {
     [self.view endEditing:YES];
-    if (!heightError && !widthError && (hasSetHeight || hasSetWidth)) {
+    if (hasSetHeight || hasSetWidth) {
         CFTypeRef canvas_height = NULL, canvas_width = NULL;
         if (!hasSetHeight) {
             canvas_height = CFPreferencesCopyValue(CFSTR("canvas_height"), CFSTR("com.apple.iokit.IOMobileGraphicsFamily"), CFSTR("mobile"), kCFPreferencesAnyHost);
